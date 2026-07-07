8a92523795f44b9f0b31d866002a2ce81e1f754d
Çağlar <83301788+caglaromurr55@users.noreply.github.com>
2026-07-07 16:00:12 +0300

    Set search_path in handle_new_user trigger to resolve enums correctly

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -470,7 +470,7 @@ BEGIN
   );
   RETURN NEW;
 END;
-$$ LANGUAGE plpgsql SECURITY DEFINER;
+$$ LANGUAGE plpgsql SECURITY DEFINER SET search_path = public, pg_temp;
 
 DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
 CREATE TRIGGER on_auth_user_created
